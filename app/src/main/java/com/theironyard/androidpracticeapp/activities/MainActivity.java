09969b17288cd45e777cb1f242332073fd089ff0
alhanger <hanger129@gmail.com>
2016-03-06 15:54:41 -0500

    Create test Show(s)

--- app/src/main/java/com/theironyard/androidpracticeapp/activities/MainActivity.java
+++ app/src/main/java/com/theironyard/androidpracticeapp/activities/MainActivity.java
@@ -2,10 +2,13 @@ package com.theironyard.androidpracticeapp.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.theironyard.androidpracticeapp.R;
 import com.theironyard.androidpracticeapp.adapters.YearListAdapter;
+import com.theironyard.androidpracticeapp.entities.Show;
 import com.theironyard.androidpracticeapp.entities.Year;
 
 public class MainActivity extends AppCompatActivity {
@@ -29,10 +32,25 @@ public class MainActivity extends AppCompatActivity {
                 new Year (2016)
         };
 
+        final Show miami = new Show("American Airlines Arena", "12/31/2009");
+        final Show charleston = new Show("North Charleston Coliseum", "10/16/2010");
+        final Show atlanta = new Show("Verizon Wireless Amphitheatre", "6/14/2011");
+        final Show colorado = new Show("Dick's Sporting Goods Park", "8/31/2012");
+        final Show hampton = new Show("Hampton Coliseum", "10/18/2013");
+        final Show orangeBeach = new Show("The Wharf", "8/1/2014");
+        final Show magnaball = new Show("Watkins Glen International", "8/23/2015");
+        final Show mexico = new Show("Riveria Maya", "1/15/2016");
+
         listView = (ListView) findViewById(R.id.list);
 
         yearListAdapter = new YearListAdapter(getApplicationContext(), years);
         listView.setAdapter(yearListAdapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
     }
 }
