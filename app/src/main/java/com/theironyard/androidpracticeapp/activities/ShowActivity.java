d6807f9cabec8ba90a49e307bdf5f3ea38b929d2
alhanger <hanger129@gmail.com>
2016-03-07 12:51:35 -0500

    Change minSdkVersion from 22 to 21

--- app/src/main/java/com/theironyard/androidpracticeapp/activities/ShowActivity.java
+++ app/src/main/java/com/theironyard/androidpracticeapp/activities/ShowActivity.java
@@ -1,19 +1,36 @@
 package com.theironyard.androidpracticeapp.activities;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.theironyard.androidpracticeapp.R;
+import com.theironyard.androidpracticeapp.adapters.ShowListAdapter;
 
 public class ShowActivity extends Activity {
 
     private ListView listView;
+    private ShowListAdapter showListAdapter;
+    private Bundle extras;
+    private TextView venueName;
+    private TextView venueLocation;
+    private TextView showDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show);
+
+        listView = (ListView) findViewById(R.id.list);
+        extras = getIntent().getExtras();
+
+        //showListAdapter = new ShowListAdapter(getApplicationContext(), );
+    }
+
+    public void getShow(String showInfo) {
+        if (showInfo.equals("showInfo")) {
+
+        }
     }
 }
